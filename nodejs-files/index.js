75dd04ccb69ce99e5a3a83a18d6b014940afa389
Daniel Ubaldo Antônio <danielubaldovrb@gmail.com>
2021-06-10 14:42:25 -0300

    Add .gitignore and package.json

--- nodejs-files/index.js
+++ nodejs-files/index.js
@@ -1,26 +1,41 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const items = await fs.readdir("stores");
-console.log(items); 
+async function findSalesFiles(folderName) {
+  // this array will hold sales files as they are found
+  let salesFiles = [];
 
-const items = await fs.readdir("stores", { withFileTypes: true });
-for (let item of items) {
-  const type = item.isDirectory() ? "folder" : "file";
-  console.log(`${item.name}: ${type}`);
-}
+  async function findFiles(folderName) {
+    // read all the items in the current folder
+    const items = await fs.readdir(folderName, { withFileTypes: true });
 
-function findFiles(folderName) {
-  const items = await fs.readdir(folderName, { withFileTypes: true });
-  items.forEach((item) => {
-    if (path.extname(item.name) === ".json") {
-      console.log(`Found file: ${item.name} in folder: ${folderName}`);
-    } else {
-      // this is a folder, so call this method again and pass in
-      // the path to the folder
-      findFiles(path.join(folderName, item.name));
+    // iterate over each found item
+    for (item of items) {
+      // if the item is a directory, it will need to be searched
+      if (item.isDirectory()) {
+        // call this method recursively, appending the folder name to make a new path
+        await findFiles(path.join(folderName, item.name));
+      } else {
+        // Make sure the discovered file is a .json file
+        if (path.extname(item.name) === ".json") {
+          // store the file path in the salesFiles array
+          salesFiles.push(path.join(folderName, item.name));
+        }
+      }
     }
-  });
+  }
+
+  await findFiles(folderName);
+
+  return salesFiles;
 }
 
-findFiles("stores");
+async function main() {
+  const salesDir = path.join(__dirname, "stores");
+
+  // find paths to all the sales files
+  const salesFiles = await findSalesFiles(salesDir);
+  console.log(salesFiles);
+}
+
+main();
